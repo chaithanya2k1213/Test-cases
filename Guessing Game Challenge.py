02899390e157c1b9f48217dae9fd7d6d43f70391
chaithanya2k1213 <83999664+chaithanya2k1213@users.noreply.github.com>
2023-11-06 22:54:33 +0530

    Add files via upload

--- Guessing Game Challenge.py
+++ Guessing Game Challenge.py
@@ -1,6 +1,13 @@
 import random
 comp_num=random.randint(1,101)
 a=[]
+print('GUESSING GAME CHALLENGE')
+print('# You have to guess a random number which is choosen by the computer.')
+print("# If you have choosen the number with less than difference of 10 of Computer's number then you will get output as WARM")
+print("# If you have choosen the number with more than difference of 10 of Computer's number then you will get output as COLD")
+print("# After getting WARM if your number is closer to the number than the previous guess, return WARMER!")
+print("# After getting WARM if your number is farther from the number than the previous guess, return COLDER!")
+print("# If your guess equals to the computer number, then it will print in how many chances u have completed the game")
 num=int(input('Guess a number between 1 and 100: '))
 if num==comp_num:
     print('Congratulations!!! You have guessed the number in {}'.format(len(a)))
@@ -12,21 +19,21 @@ elif num!=comp_num:
         a.append(num)
         print('COLD')
     while 1:
-        num=int(input('Guess a number between 1 and 1002: '))
+        num=int(input('Guess a number between 1 and 100: '))
         a.append(num)
         if num==comp_num:
             print('Congratulations!!! You have guessed the number in {}'.format(len(a)))
             break
-        #1
+        
         if num>100 or  num<1:
             print('OUT OF BOUNDS')
             
         
-        #2
+        
         if (comp_num+10>num) and (num>comp_num-10):
             
             print('WARM')
-            #3
+            
             while 1:
                 num=int(input('Guess a number between 1 and 100: '))
                 a.append(num)
